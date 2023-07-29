6026a53400e37f51d3c3336d274e007dcfc97823
Joffreynk <joyjoffrey@gmail.com>
2023-07-29 11:57:17 +0200

    add nav

---
+++ components/ui/Container.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Container({children}: {children: React.ReactNode}) {
+  return (
+    <div className='mx-auto max-w-7xl'>
+      {children}
+    </div>
+  )
+}
+
+export default Container
