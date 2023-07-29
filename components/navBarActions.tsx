3ab9ae399eb9b6e88923187def2c7880105bb2eb
Joffreynk <joyjoffrey@gmail.com>
2023-07-29 18:41:49 +0200

    create nav bar actions

---
+++ components/navBarActions.tsx
@@ -0,0 +1,12 @@
+import Button from "./ui/Button";
+
+export default  function navBarActions() {
+  return (
+    <div className="ml-auto flex items-center gap-x-4">
+        <Button >
+            gfgfgff
+        </Button>
+    </div>
+  )
+}
+
