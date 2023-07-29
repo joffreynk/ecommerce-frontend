b50e78d24221d84166b0da03316b3351cbaa88de
Joffreynk <joyjoffrey@gmail.com>
2023-07-29 21:33:25 +0200

    finalize button

--- components/navBarActions.tsx
+++ components/navBarActions.tsx
@@ -17,10 +17,10 @@ export default function NavBarActions() {
     <div className="ml-auto flex items-center gap-x-4">
       <Button className="flex relative items-center rounded-full bg-slate-900">
         <ShoppingBag
-          size={30}
+          size={25}
           color="white"
          />
-         <span className="text-md font-medium absolute text-gray-50 right-[8px] top-1">10</span>
+         <span className="text-md font-medium absolute text-gray-50 right-[6px] top-1">10</span>
       </Button>
     </div>
   );
--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Container({children}: {children: React.ReactNode}) {
   return (
-    <div className='mx-auto max-w-7xl'>
+    <div className='mx-auto max-w-[1366px]'>
       {children}
     </div>
   )
