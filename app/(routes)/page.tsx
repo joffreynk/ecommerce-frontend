683e08fd87b1d1b4d6d119ebfa7ec3e4bdc059ce
Joffreynk <joyjoffrey@gmail.com>
2023-07-30 11:44:57 +0200

    finalize sing product

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -5,17 +5,17 @@ import ProductList from "@/components/ProductList";
 import Container from "@/components/ui/Container";
 
 export default async function Home() {
-  const billboard = await getBillboard('69f00970-f66d-44df-acba-9c6f7f145925')
-  const products = await getProducts({isFeatured: true})
+  const billboard = await getBillboard("69f00970-f66d-44df-acba-9c6f7f145925");
+  const products = await getProducts({ isFeatured: true });
 
   return (
-    <Container >
-      <div className="space-y-10 pb-10">
+    <Container>
+      <div className="space-y-10 pb-10 lg:pb-16">
         <BillboardPage data={billboard} />
-      </div>
-      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <ProductList title="Featured Products" items={products} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
-  )
+  );
 }
