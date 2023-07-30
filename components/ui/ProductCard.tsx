15f0bfe0e59e22866613fb98f295e728762e8dd0
Joffreynk <joyjoffrey@gmail.com>
2023-07-30 11:17:59 +0200

    add product details

--- components/ui/ProductCard.tsx
+++ components/ui/ProductCard.tsx
@@ -3,21 +3,39 @@
 import { Product } from '@/types'
 import Image from 'next/image'
 import React from 'react'
+import IconButton from './IconButton'
+import { Expand, ShoppingCart } from 'lucide-react'
 
 function ProductCard({data}: {data: Product}) {
   return ( 
     <div className='bg-white group cursor-pointer rounded-xl border p-3 space-y-4'>
+      {/* product  images and actions */}
          <div className='aspect-square rounded-xl bg-gray-100 relative'>
             <Image src={data.images[0].url} alt='product picture' fill
             className='aspect-square object-cover rounded-md'
              />
 
-             <div className='opacity-0 group-hover:opacity-100'>
+             <div className='opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-4'>
                 <div className='flex gap-x-6 justify-center'>
-                    
+                    <IconButton
+                    onClick={()=>{}}
+                      icon={<Expand  size={20} className='text-gray-600' />}
+                      />
+                      <IconButton
+                    onClick={()=>{}}
+                      icon={<ShoppingCart  size={20} className='text-gray-600' />}
+                      />
                 </div>
              </div>
          </div>
+    {/* product description and price */}
+    <div className='flex gap-2 justify-between'>
+      <div>
+        <p className='font-semibold text-lg'>{data.name}</p>
+        <p className='text-sm text-gray-500'>{data.category.name}</p>
+      </div>
+      <p className='font-semibold self-center'>${data.price}</p>
+    </div>
     </div>
   )
 }
