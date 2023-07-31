2f44c5a1071f4dd2e521f431410fbbc2f1d96cee
Joffreynk <joyjoffrey@gmail.com>
2023-07-31 22:07:54 +0200

    get simple component

--- app/(routes)/[categoryId]/[productId]/page.tsx
+++ app/(routes)/[categoryId]/[productId]/page.tsx
@@ -2,8 +2,7 @@ import getProducts from '@/actions/getProducts';
 import getproduct from '@/actions/getproduct';
 import ProductList from '@/components/ProductList'
 import Container from '@/components/ui/Container'
-import Gallery from '@/components/Gallery';
-import ProductInfo from '@/components/ProductInfo';
+import ProductComponent from '@/components/ui/ProductComponent';
 
 const SingleProduct = async({params}: {params: {productId: string}}) => {
 
@@ -19,14 +18,7 @@ const SingleProduct = async({params}: {params: {productId: string}}) => {
        <Container>
     <div className="px-4 py-10 sm:px-6 lg:px-8">
       <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <div className='grid lg:grid-cols-2 lg:items-start lg:gap-x-8'>
-          {/* Gallery */}
-          <Gallery images={product.images} />
-          <div>
-            {/* Info */}
-            <ProductInfo data={product} />
-          </div>
-        </div>
+        <ProductComponent product={product} />
         <hr className='my-10 w-full' />
         <ProductList title={`${product?.category?.name}'s related products`} items={products} />
       </div>
