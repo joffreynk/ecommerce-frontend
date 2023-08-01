a97e9872aa1bd45f1a78fa9ae8366d7c400ba8ed
Joffreynk <joyjoffrey@gmail.com>
2023-08-01 23:48:28 +0200

    finalize cart

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -6,6 +6,7 @@ import { useSearchParams } from "next/navigation"
 import Button from "./ui/Button"
 import Currency from "./ui/Currency"
 import useCart from "@/hooks/useCart";
+import { toast } from "react-hot-toast";
 
 const Summary = () => {
     const searchParams = useSearchParams();
@@ -13,10 +14,30 @@ const Summary = () => {
     const removeAll = useCart((state)=>state.removeAll);
 
     useEffect(() => {
+        if(searchParams.get('success')){
+            toast.success("payment successed");
+            removeAll();
+        }
 
-    }, [])
+        if(searchParams.get('canceled')){
+            toast.success("Something went wrong");
+        }
+    }, [searchParams, removeAll])
 
     const totalPrice = items.reduce((total, item)=>total+Number( item.price), 0);
+
+    const checkout = async()=>{
+        const options = {
+            method: "POST",
+            body: JSON.stringify({
+                productIds: items.map((item)=>item.id)
+            })
+        }
+
+        const response = await fetch(`${process.env.NEXT_STORE_PUBLIC_API}/checkout`, options);
+        const result = await response.json()
+        window.location = result.url;
+    }
     
   return (
     <div className="lg:ml-6 mt-5 lg:mt-0 rounded-xl p-4 lg:col-span-5 bg-slate-100 gap-4 flex flex-col w-full">
@@ -30,7 +51,7 @@ const Summary = () => {
       </div>
       <div>
         
-        <Button className="w-full bg-black text-white text-xl py-3 rounded-3xl" >Checkout</Button>
+        <Button onClick={checkout} className="w-full bg-black text-white text-xl py-3 rounded-3xl" >Checkout</Button>
       </div>
     </div>
   )
