77d4e8f963c243966573d8c718ad19df546d269b
Joffreynk <joyjoffrey@gmail.com>
2023-08-01 20:46:51 +0200

    add cart style

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -15,6 +15,11 @@ const CartPage = () => {
     <Container >
       <div className="px-4 py-16 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
+        <div className="mt-12 lg:grig lg:grid-cols-12 lg:items-start">
+          <div className="lg:col-span-7">
+            {cart.items.length === 0 && <p className="text-neutral-500">No Items added in the cart</p>}
+          </div>
+        </div>
       </div>
     </Container>
   </div>);
--- components/navBarActions.tsx
+++ components/navBarActions.tsx
@@ -5,10 +5,13 @@ import { useEffect, useState } from "react";
 
 import Button from "./ui/Button";
 import useCart from "@/hooks/useCart";
+import { useRouter } from "next/navigation";
 
 export default function NavBarActions() {
   const cart = useCart();
   const [isMountend, setIsMountend] = useState(false)
+  const router  = useRouter();
+
   useEffect(()=>{
     setIsMountend(true)
   }, [])
@@ -17,7 +20,7 @@ export default function NavBarActions() {
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className="flex gap-1 items-center p-2 rounded-full bg-slate-900">
+      <Button className="flex gap-1 items-center p-2 rounded-full bg-slate-900" onClick={()=>router.push('/cart')}>
         <ShoppingBag
           size={25}
           color="white"
