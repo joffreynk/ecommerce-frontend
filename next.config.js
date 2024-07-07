04b7ac45c3359242328e0eaa553bd86c1c19cb14
joffreynk <joyjoffrey@gmail.com>
2024-07-07 20:41:39 +0200

    disable typescript at building time

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["res.cloudinary.com"],
-      },
-}
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
 module.exports = nextConfig
